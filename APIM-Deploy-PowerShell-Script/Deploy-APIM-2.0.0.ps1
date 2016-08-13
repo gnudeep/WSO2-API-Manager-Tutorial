4f7b84c51a4a82dc55cb218d3ac1f0968e8c10e5
Deependra Ariyadewa <deependra@gmail.com>
2016-08-13 13:35:24 +0000

    APIM-Deploy-PowerShell-Script added

---
+++ APIM-Deploy-PowerShell-Script/Deploy-APIM-2.0.0.ps1
@@ -0,0 +1,34 @@
+﻿Write-Host WSO2 APIM 2.0.0 Deployment Started.
+
+If ($args.Count -lt 3)
+{
+Write-Host Syntax : c:\Deploy-APIM-2.0.0 ArtifactPath APIManagerHome
+}
+
+$ArtifactFullPath=$args[0]
+$APIManagerDeploymentPath=$args[1]
+
+$APIArtifactName="wso2am-2.0.0.zip"
+$JDBCDriver="sqljdbc4.jar"
+$KeyStore="wso2carbon.jks"
+$TrustStore="client-truststore.jks"
+
+$APIMFolder="wso2am-2.0.0"
+$APIMLibPath = "\repository\components\lib"
+$APIMJKSPath = "\repository\resources\security"
+
+#Unzip the APIM Artifact
+Write-Host Unziping $APIArtifactName
+Expand-Archive -LiteralPath $ArtifactFullPath\$APIArtifactName -DestinationPath $APIManagerDeploymentPath
+
+#Copy JDBC dirver 
+Write-Host Coping $JDBCDriver
+Copy-Item $ArtifactFullPath\$JDBCDriver $APIManagerDeploymentPath\$APIMFolder\$APIMLibPath
+
+#Copy Keystore
+Write-Host Copying $KeyStore
+Copy-Item $ArtifactFullPath\$KeyStore $APIManagerDeploymentPath\$APIMFolder\$APIMJKSPath
+
+#Copy Truststore
+Write-Host Copying $TrustStore
+Copy-Item $ArtifactFullPath\$TrustStore $APIManagerDeploymentPath\$APIMFolder\$APIMJKSPath
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # WSO2 API Manager Tutorial
 
 1. [JWT Claim Extractor](https://github.com/gnudeep/WSO2-API-Manager-Tutorial/tree/master/JWT-Claim-Extractor)
+2. [API Change Notification Handler](https://github.com/gnudeep/WSO2-API-Manager-Tutorial/tree/master/API-Change-Notification-Handler)
+3. [API Manager Deployment PowerShell Script]()
